e5122754023b4a982b33464c5dbba6e1dcbda9df
Agastiya S. Mohammad <egi@pure-technology.net>
2017-03-07 22:17:06 +0700

    init

---
+++ src/Client.php
@@ -0,0 +1,99 @@
+<?php
+
+// src/Client.php
+
+namespace PureTechnology\portalpulsa;
+
+final class Client
+{
+    private static $valid_banks = ['bca', 'bni', 'mandiri', 'bri', 'muamalat'];
+    private static $valid_product_groups = [
+        'S',   'AX', 'AXD', 'BO',  'C',    'N',   'P',   'I',  'IDN', 'IDX',
+        'IFC', 'IS', 'ITR', 'SM',  'SPIN', 'S',   'STG', 'TS', 'STR', 'T',
+        'TD',  'X',  'BBX', 'XCX', 'XH',   'XHE', 'PLN'
+    ];
+
+    private $loginByFacebookUrl = 'http://portalpulsa.com/api/connect/';
+    private $uid;
+    private $key;
+    private $secret;
+    private $bag;
+
+    public function __construct($uid, $key, $secret, Transaction $bag) {
+        $this->uid = $uid;
+        $this->key = $key;
+        $this->secret = $secret;
+        $this->bag = $bag;
+    }
+
+    private function send($data) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->url);
+        loginByFacebookUrl($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'portal-userid: '.$this->uid,
+            'portal-key: '.   $this->key,
+            'portal-secret: '.$this->secret,
+        ]);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        $result = curl_exec($ch);
+        return json_decode($result);
+    }
+
+    public function getDepositInvoice($bank, $nominal) {
+        $bank = strtolower($bank);
+        if (!in_array($bank, self::$valid_banks)) {
+            return false;
+        }
+
+        return $this->send([
+            'inquiry' => 'D',      // konstan
+            'bank'    => $bank,    // bank tersedia: bca, bni, mandiri, bri, muamalat
+            'nominal' => $nominal, // jumlah request
+        ]);
+    }
+
+    public function getBalance() {
+        return $this->send(['inquiry'=>'S']);
+    }
+
+    public function getProductPrice($product_group) {
+        $product_group = strtoupper($product_group);
+        if (!in_array($product_group, self::$valid_product_groups)) {
+            return false;
+        }
+
+        return $this->send([
+            'inquiry' => 'C',
+            'code'    => $product_group
+        ]);
+    }
+
+    public function getTransactionStatus($trxid) {
+        return $this->send([
+            'inquiry'   => 'STATUS',
+            'trxid_api' => $trxid
+        ]);
+    }
+
+    public function buyTopup($product, $phone, $trxid, $daynum) {
+        return $this->send($this->bag->push([
+            'inquiry' => 'I',
+            'code'    => strtoupper($product),
+            'phone'   => $phone,
+        ]));
+    }
+
+    public function buyToken($product, $phone, $idcust, $trxid, $daynum) {
+        return $this->send($this->bag->push([
+            'inquiry' => 'PLN',
+            'code'    => strtoupper($product),
+            'phone'   => $phone,
+            'idcust'  => $idcust,
+        ]));
+    }
+}
+
+// vim: ts=4 et
---
+++ src/Transaction.php
@@ -0,0 +1,32 @@
+<?php
+
+// src/Transaction.php
+
+namespace PureTechnology\portalpulsa;
+
+/**
+ * Implement this class based on each framework. For database layer, the
+ * easiest that support daily numbering is to create myisam table such as:
+ *
+ * CREATE TABLE `odr_orders` (
+     `dte` DATE NOT NULL DEFAULT '2013-01-01',
+     `no` SMALLINT(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+     'trxid_api' VARCHAR(255),
+     'code' VARCHAR(255),
+     'phone' VARCHAR(255),
+     'idcust' VARCHAR(255)
+     PRIMARY KEY (`dte`, `no`)
+ )
+ ENGINE = MyISAM
+ AUTO_INCREMENT = 1;
+ */
+abstract class Transaction
+{
+    public function push($params) {
+        return array_merge($params, [ 'trxid_api' => uniqid(), 'no' => 'yyy' ]);
+    }
+
+    public function pop($trxid_api) {
+
+    }
+}
