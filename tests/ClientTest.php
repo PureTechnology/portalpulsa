71af3bfc2194f20d304e8973d08bec72dc716a71
Agastiya S. Mohammad <egi@pure-technology.net>
2017-03-14 17:08:05 +0700

    added topup testcase

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -7,18 +7,50 @@ namespace PureTechnology\portalpulsa\Test;
 use PureTechnology\portalpulsa\Client;
 
 class ClientTest extends \PHPUnit_Framework_TestCase {
+
+    protected $queue;
+    protected function setUp()
+    {
+        $this->queue = $this->getMockBuilder('\PureTechnology\portalpulsa\AbstractQueue')
+            ->getMock();
+    }
+
     public function testGetBalance()
     {
-        $queue = $this->getMockBuilder('\PureTechnology\portalpulsa\AbstractQueue')
-            ->getMock();
-
-        $c = new Client(
+        $client = new Client(
             getenv('T_UID'),
             getenv('T_KEY'),
             getenv('T_SECRET'),
-            $queue
+            $this->queue
         );
 
-        $this->assertEquals(array(), $c->getBalance());
+        $result = $client->getBalance();
+        $this->assertEquals('success', $result['result']);
+        $this->assertContains('Sisa saldo Anda saat ini', $result['message']);
+        $this->assertArrayHasKey('balance', $result);
+    }
+
+    public function testBuyTopupSingleProductMatching()
+    {
+        $this->queue->expects($this->once())
+            ->method('push')
+            ->will($this->returnValue(array(
+                'inquiry'   => 'I',
+                'code'      => 'S5',
+                'phone'     => '081234567',
+                'trxid_api' => 'xxx',
+                'no'        => '1',
+            )));
+
+        $client = new Client(
+            getenv('T_UID'),
+            getenv('T_KEY'),
+            getenv('T_SECRET'),
+            $this->queue
+        );
+
+        $result = $client->buyTopup('S5', '081234567');
+        $this->assertEquals('success', $result['result']);
+        $this->assertEquals('S5 081234567 Akan diproses', $result['message']);
     }
 }
